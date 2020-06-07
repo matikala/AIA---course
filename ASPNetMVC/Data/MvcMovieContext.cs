8c14925bffec2405b9ad5d3374545829cb2b7aa1
Mateusz Kałamoniak <31251302+matikala@users.noreply.github.com>
2020-06-07 19:52:58 +0200

    Add files via upload

---
+++ ASPNetMVC/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ASPNetMVC.Models;
+
+namespace ASPNetMVC.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
